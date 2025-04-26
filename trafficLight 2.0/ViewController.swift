57d7b2f0f474a5321d0f22da1835c533488b7211
LeonidPirogov <lv.pirogof@gmail.com>
2025-04-26 17:17:31 +0300

    Initial Commit

---
+++ trafficLight 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight 2.0
+//
+//  Created by Leonid on 26.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
